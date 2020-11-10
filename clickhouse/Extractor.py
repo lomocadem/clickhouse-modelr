ac2c706c91b050baa9e7d8bff114d1fd75b2d705
Calvin <calvin@paymentwall.com>
2020-11-10 12:14:21 +0700

    Version 0.4
    Signed-off-by: Calvin <calvin@paymentwall.com>

--- clickhouse/Extractor.py
+++ clickhouse/Extractor.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 from pandas import DataFrame
 
 json_schema_location = r"C:/Users/calvin/PycharmProjects/clickhouse-modelr/sample.json"
@@ -30,8 +31,33 @@ class Columns:
     def __init__(self, columns):
         self.columns = columns
 
+    @staticmethod
+    def constructNullable(d_type, is_nullable):
+        if is_nullable:
+            data_type = f"Nullable({d_type})"
+        else:
+            data_type = d_type
+        return data_type
+
+    @staticmethod
+    def constructCodec(codec_name):
+        if isinstance(codec_name, str):
+            codec = ""
+        else:
+            codec = f"CODEC({codec_name})"
+        return codec
+
+    @staticmethod
+    def constructTimezone(time_type, timezone):
+        if isinstance(timezone, str):
+            time_with_tz = time_type
+        else:
+            time_with_tz = f"{time_type}('{timezone}')"
+        return time_with_tz
+
     @classmethod
     def constructColumns(cls, columns_dict: dict):
+        # Convert column dict to dataframe
         column_df = DataFrame.from_dict(columns_dict, orient='index',
                                         columns=['type',
                                                  'is_primary_key',
@@ -42,9 +68,23 @@ class Columns:
                                                  'compression_codec',
                                                  'function',
                                                  'time_zone'])
+        # Rename column_name
         column_with_name = column_df.reset_index().rename(columns={'index': 'column_name'})
 
-        return cls(column_with_name)
+        column_list = list()
+        t_list = list()
+        # Construct columns DDL
+        for index, row in column_with_name.iterrows():
+            column_name = row['column_name']
+            codec = Columns.constructCodec(row['compression_codec'])
+            time = Columns.constructTimezone(row['type'], row['time_zone'])
+            data_type = Columns.constructNullable(row['type'], row['is_nullable'])
+            each = f"{column_name} {data_type} {codec}"
+            # each = f"{row['column_name']} {row['type']} CODEC({row['compression_codec']})"
+            column_list.append(each)
+            t = row['compression_codec']
+            t_list.append(t)
+        return cls(t_list)
 
 
 # Construct table: db.table
@@ -64,7 +104,7 @@ class Engine:
         self.engine = engine
 
     @classmethod
-    def constructEngine(cls,engine_name):
+    def constructEngine(cls, engine_name):
         engine = "ENGINE=" + engine_name + "()"
         return cls(engine)
 
@@ -99,12 +139,8 @@ class SortingKey:
         return cls(sorting_key)
 
 
-
-
-
 a = Extractor.extractJson(json_schema_location)
 print(type(a.columns))
 
 b = Columns.constructColumns(a.columns)
 print(b.columns)
-
